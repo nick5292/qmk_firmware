aa8099833f8e9fd6cd1b9f4eb365d9bba5879df9
nick5292 <nick@meridionalis.com>
2026-04-08 11:17:15 +0900

    Update rules.mk

--- keyboards/aki27/trackball_module/keymaps/default/rules.mk
+++ keyboards/aki27/trackball_module/keymaps/default/rules.mk
@@ -1,1 +1,11 @@
+# マウス機能（スクロール等）を有効にする（超重要！）
+MOUSEKEY_ENABLE = yes
+
+# エンコーダー機能を有効にする
+ENCODER_ENABLE = yes
+
+# Remapや新しいQMKの設定形式(encoder_map)を使うために有効化
+ENCODER_MAP_ENABLE = yes
+
+# VIA/Remapで動的に変えたい場合はこれもyesにする（容量が足りれば）
 VIA_ENABLE = yes
